ec01d56c33344ebd1c8eaa5200c09bcd31f4bde5
Ανδρέας Μανίτσας <andreasmanitsas@MacBook-Air-Andreas.local>
2020-01-23 12:51:52 +0200

    added array matching function and started BFS implementation

--- main.c
+++ main.c
@@ -4,7 +4,6 @@
 
 #include "misc.h"
 #include "search.h"
-#include "queue.h"
 
 unsigned int nodesExpanded = 0;
 unsigned int nodesGenerated = 0;
@@ -23,60 +22,6 @@ int main ()
 
 	randArrayInitialization(*initialArray);
 	BFSSearch(*initialArray, *targetArray);
-	//testNode(*initialArray, *targetArray);
-	//testQueue(*initialArray, *targetArray);
-
-	/*
-	for (i = 1; i < 5; i++)
-	{
-		possibleMove = validMovement(*initialArray, i);
-
-		if (i == 1) //UP
-		{
-			if (possibleMove == 1)
-			{
-				printf("Possible UP\n");
-			}
-			else
-			{
-				printf("NOT possible UP\n");
-			}
-		}
-		else if (i == 2) //RIGHT
-		{
-			if (possibleMove == 1)
-			{
-				printf("Possible RIGHT\n");
-			}
-			else
-			{
-				printf("NOT possible RIGHT\n");
-			}
-		}
-		else if (i == 3) //DOWN
-		{
-			if (possibleMove == 1)
-			{
-				printf("Possible DOWN\n");
-			}
-			else
-			{
-				printf("NOT possible DOWN\n");
-			}
-		}
-		else if (i == 4) //LEFT
-		{
-			if (possibleMove == 1)
-			{
-				printf("Possible LEFT\n");
-			}
-			else
-			{
-				printf("NOT possible LEFT\n");
-			}
-		}
-	}
-	*/
 	
 	return 0;
 }
--- search.h
+++ search.h
@@ -1,6 +1,14 @@
 #if !defined(SEARCH)
 #define SEARCH
 
+struct Node *newNode(int *, struct Node *, int , int , int);
+struct Queue *createQueue();
+void addToQueue(struct Queue *, struct Node *);
+struct Node *removeFromQueue(struct Queue *);
+int validMovement(int *, int);
+void testNode(int *, int *); //Used only for debugging and testing
+void testQueue(int *, int *); //Used only for debugging and testing
+int arrayMatching(int *, int *);
 void BFSSearch(int *, int *);
 void AstarSearch(int *, int *);
 
